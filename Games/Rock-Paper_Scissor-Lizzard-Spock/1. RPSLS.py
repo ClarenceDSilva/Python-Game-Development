b46bc386bab16d43b9d71f124f5529b155324653
Clarence D'Silva <clarence.dsilva@hotmail.com>
2019-06-15 13:04:07 -0400

    Update print statements

--- Games/Rock-Paper_Scissor-Lizzard-Spock/1. RPSLS.py
+++ Games/Rock-Paper_Scissor-Lizzard-Spock/1. RPSLS.py
@@ -15,7 +15,7 @@ def name_to_number(name):
     elif name== "scissors":
         name=4
     else:
-        print "Invalid Selection!!"
+        print ("Invalid Selection!!")
     return name    
 
 #Conversion of number to name
@@ -32,14 +32,14 @@ def number_to_name(number):
     elif number==4:
         number= "Scissors"
     else:
-        print "Please enter the correct number."
+        print ("Please enter the correct number.")
     return number    
     
 #Main Fundtion, Computing Rock-Paper-Scissors-Lizard-Spock
 def rpsls(player_choice): 
     
-    print ""
-    print "Player chooses",player_choice
+    print ("")
+    print ("Player chooses",player_choice)
     
     choosen = name_to_number(player_choice)
     player_number= choosen
@@ -48,18 +48,18 @@ def rpsls(player_choice):
     comp_number= random.randrange(0,5)
     comp_choice= number_to_name(comp_number)
     
-    print "Computer chooses",comp_choice
+    print ("Computer chooses",comp_choice)
     
     compute_difference= (player_number-comp_number)% 5
     
     if (compute_difference < 3) and (compute_difference>0):
-        print "Player Wins!"
+        print ("Player Wins!")
     elif compute_difference >= 3:
-        print "Computer Wins!"
+        print ("Computer Wins!")
     elif compute_difference == 0:
-        print "Player and Computer tie!"
+        print ("Player and Computer tie!")
     else:
-        print "Methodological Error!!"
+        print ("Methodological Error!!")
         
         
 # Function Call 
